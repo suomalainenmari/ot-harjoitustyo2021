7b88c9a79bca7ceb6d638b3b9077035571db1562
Mari Suomalainen <mariannikasuomalainen@gmail.com>
2021-04-13 16:30:49 +0300

    Refaktoroitu coverage-report taski kahdeksi eri taskiksi

--- Laskin/tasks.py
+++ Laskin/tasks.py
@@ -8,7 +8,10 @@ def start(ctx):
 def test(ctx):
   ctx.run("pytest")
 
+@task 
+def coverage(ctx):
+  ctx-run("coverage run --branch -m pytest src")
+
 @task
 def coverage_report(ctx):
-  ctx.run("coverage run --branch -m pytest src")
   ctx.run("coverage report -m")
