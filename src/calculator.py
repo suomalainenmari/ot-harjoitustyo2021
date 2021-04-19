7ac7def1c5178d9da1efdf7c76151a118b182cbd
Mari Suomalainen <mariannikasuomalainen@gmail.com>
2021-04-19 16:06:38 +0300

    Pylint konfiguraatiot

--- src/calculator.py
+++ src/calculator.py
@@ -1,22 +1,21 @@
 class Calculator:
-  def __init__(self, result=0.0):
-    self.result = result
+    def __init__(self, result=0.0):
+        self.result = result
 
-  def add(self,value):
-    self.result = self.result+value
+    def add(self, value):
+        self.result = self.result+value
 
-  def subtract(self,value):
-    self.result=self.result-value
+    def subtract(self, value):
+        self.result = self.result-value
 
-  def multiply(self,value):
-    self.result=self.result*value
+    def multiply(self, value):
+        self.result = self.result*value
 
-  def divide(self,value):
-    if value==0:
-      self.result="Error"
-    else:
-      self.result = self.result/value
+    def divide(self, value):
+        if value == 0:
+            self.result = "Error"
+        else:
+            self.result = self.result/value
 
-  def clear(self):
-    self.result = 0.0
-
+    def clear(self):
+        self.result = 0.0
--- src/user_interface.py
+++ src/user_interface.py
@@ -1,92 +1,94 @@
-from tkinter import Tk, ttk, constants, IntVar, DoubleVar
+from tkinter import Tk, ttk, constants, DoubleVar
 from calculator import Calculator
 
+
 class UI:
-  def __init__(self,root):
-    self._root = root
-    self._entry = None
-    self._calculator = Calculator()
-  
-  def start(self):
-    self._result_var = DoubleVar()
-    self._result_var.set(self._calculator.result)
+    def __init__(self, root):
+        self._root = root
+        self._entry = None
+        self._calculator = Calculator()
+        self._result_var = DoubleVar()
 
-    header_label = ttk.Label(master=self._root, text="Laskin")
-    input_label = ttk.Label(master=self._root, text="Syöte:")
-    self._entry = ttk.Entry(master=self._root)
-    
-    plus_button = ttk.Button(
-      master=self._root,
-       text="+",
-       command= self._handle_plus_click
-       )
+    def start(self):
+        self._result_var.set(self._calculator.result)
 
-    minus_button = ttk.Button(
-      master=self._root,
-      text="-",
-      command= self._handle_minus_click
-      )
+        header_label = ttk.Label(master=self._root, text="Laskin")
+        input_label = ttk.Label(master=self._root, text="Syöte:")
+        self._entry = ttk.Entry(master=self._root)
 
-    multiply_button = ttk.Button(
-      master=self._root,
-      text="*",
-      command= self._handle_multiply_click
-      )
-    
-    divide_button = ttk.Button(
-      master=self._root,
-      text="/",
-      command= self._handle_divide_click
-      )
+        plus_button = ttk.Button(
+            master=self._root,
+            text="+",
+            command=self._handle_plus_click
+        )
 
-    all_clear_button = ttk.Button(
-      master=self._root,
-      text="AC",
-      command=self._handle_all_clear_click
-    )
-    
-    output_label = ttk.Label(master=self._root, text="Tulos:")
-    output_itself = ttk.Label(master=self._root, textvariable=self._result_var)
+        minus_button = ttk.Button(
+            master=self._root,
+            text="-",
+            command=self._handle_minus_click
+        )
+
+        multiply_button = ttk.Button(
+            master=self._root,
+            text="*",
+            command=self._handle_multiply_click
+        )
+
+        divide_button = ttk.Button(
+            master=self._root,
+            text="/",
+            command=self._handle_divide_click
+        )
+
+        all_clear_button = ttk.Button(
+            master=self._root,
+            text="AC",
+            command=self._handle_all_clear_click
+        )
+
+        output_label = ttk.Label(master=self._root, text="Tulos:")
+        output_itself = ttk.Label(
+            master=self._root, textvariable=self._result_var)
+
+        header_label.grid(row=0, column=0, columnspan=2)
+        input_label.grid(row=1, column=0, columnspan=2)
+        self._entry.grid(row=2, column=0, columnspan=2)
+        plus_button.grid(row=3, column=0)
+        minus_button.grid(row=3, column=1)
+        multiply_button.grid(row=4, column=0)
+        divide_button.grid(row=4, column=1)
+        output_label.grid(row=6, column=0)
+        output_itself.grid(row=6, column=1)
+        all_clear_button.grid(row=5, columnspan=2, sticky=constants.W)
+
+    def _handle_plus_click(self):
+        entry_value = self._entry.get()
+        self._calculator.add(float(entry_value))
+        self._result_var.set(self._calculator.result)
+
+    def _handle_minus_click(self):
+        entry_value = self._entry.get()
+        self._calculator.subtract(float(entry_value))
+        self._result_var.set(self._calculator.result)
+
+    def _handle_multiply_click(self):
+        entry_value = self._entry.get()
+        self._calculator.multiply(float(entry_value))
+        self._result_var.set(self._calculator.result)
+
+    def _handle_divide_click(self):
+        entry_value = self._entry.get()
+        self._calculator.divide(float(entry_value))
+        self._result_var.set(self._calculator.result)
+
+    def _handle_all_clear_click(self):
+        self._calculator.clear()
+        self._result_var.set(self._calculator.result)
 
 
-    header_label.grid(row=0, column=0, columnspan=2)
-    input_label.grid(row=1, column=0,columnspan=2)
-    self._entry.grid(row=2,column=0,columnspan=2)
-    plus_button.grid(row=3,column=0)
-    minus_button.grid(row=3,column=1)
-    multiply_button.grid(row=4, column=0)
-    divide_button.grid(row=4, column=1)
-    output_label.grid(row=6,column=0)
-    output_itself.grid(row=6, column=1)
-    all_clear_button.grid(row=5,columnspan=2, sticky=constants.W)
-
-  def _handle_plus_click(self):
-    entry_value = self._entry.get()
-    self._calculator.add(float(entry_value))
-    self._result_var.set(self._calculator.result)
-
-  def _handle_minus_click(self):
-    entry_value = self._entry.get()
-    self._calculator.subtract(float(entry_value))
-    self._result_var.set(self._calculator.result)
-
-  def _handle_multiply_click(self):
-    entry_value = self._entry.get()
-    self._calculator.multiply(float(entry_value))
-    self._result_var.set(self._calculator.result)
-
-  def _handle_divide_click(self):
-    entry_value = self._entry.get()
-    self._calculator.divide(float(entry_value))
-    self._result_var.set(self._calculator.result)
-
-  def _handle_all_clear_click(self):
-    self._calculator.clear()
-    self._result_var.set(self._calculator.result)
-
-window = Tk()  
+window = Tk()
 window.title("Laskin")
 
-ui= UI(window)
+ui = UI(window)
 ui.start()
 window.mainloop()
