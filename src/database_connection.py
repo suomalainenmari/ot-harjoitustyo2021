bb8b588c7a75282a2000f926dd21844a1c5b5e64
Mari Suomalainen <mariannikasuomalainen@gmail.com>
2021-05-16 13:23:29 +0300

    Tietokanta ei tyhjene uudelleen käynnistettäessä

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,10 +1,7 @@
-import os
 import sqlite3
 
-dirname = os.path.dirname(__file__)
-
-connection = sqlite3.connect(os.path.join(dirname, "..", "notes.db"))
-
+connection = sqlite3.connect("notes.db")
+connection.isolation_level = None
 
 def get_database_connection():
     return connection
